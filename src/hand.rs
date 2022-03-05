0aa8dfa54b30a7a5975ca2bd5c4a12bed8842bf8
sugyan <sugi1982@gmail.com>
2022-03-05 13:55:27 +0900

    Update hands

--- src/hand.rs
+++ src/hand.rs
@@ -1,57 +1,41 @@
 use crate::PieceType;
 
+// [0: FU, 1: KY, 2: KE, 3: GI, 4: KI, 5: KA, 6: HI]
 #[derive(Clone, Copy, Debug, Default)]
-pub struct Hand {
-    fu: u8,
-    ky: u8,
-    ke: u8,
-    gi: u8,
-    ki: u8,
-    ka: u8,
-    hi: u8,
-}
+pub struct Hand([u8; PieceType::NUM_HAND]);
 
 impl Hand {
+    const PIECE_TYPE_INDEX: [usize; PieceType::NUM] = [
+        PieceType::NUM, // OCCUPIED => unreachable!
+        0,              // FU
+        1,              // KY
+        2,              // KE
+        3,              // GI
+        5,              // KA
+        6,              // HI
+        4,              // KI
+        PieceType::NUM, // OU => unreachable!
+        0,              // TO
+        1,              // NY
+        2,              // NK
+        3,              // NG
+        5,              // UM
+        6,              // RY
+    ];
+
     pub fn new() -> Self {
         Self::default()
     }
     pub fn is_empty(&self) -> bool {
-        self.fu + self.ky + self.ke + self.gi + self.ki + self.ka + self.hi == 0
+        self.0[0] + self.0[1] + self.0[2] + self.0[3] + self.0[4] + self.0[5] + self.0[6] == 0
     }
     pub fn num(&self, pt: PieceType) -> u8 {
-        match pt {
-            PieceType::FU => self.fu,
-            PieceType::KY => self.ky,
-            PieceType::KE => self.ke,
-            PieceType::GI => self.gi,
-            PieceType::KI => self.ki,
-            PieceType::KA => self.ka,
-            PieceType::HI => self.hi,
-            _ => unreachable!(),
-        }
+        self.0[Hand::PIECE_TYPE_INDEX[pt.index()]]
     }
     pub fn increment(&mut self, pt: PieceType) {
-        match pt {
-            PieceType::FU | PieceType::TO => self.fu += 1,
-            PieceType::KY | PieceType::NY => self.ky += 1,
-            PieceType::KE | PieceType::NK => self.ke += 1,
-            PieceType::GI | PieceType::NG => self.gi += 1,
-            PieceType::KI => self.ki += 1,
-            PieceType::KA | PieceType::UM => self.ka += 1,
-            PieceType::HI | PieceType::RY => self.hi += 1,
-            _ => unreachable!(),
-        }
+        self.0[Hand::PIECE_TYPE_INDEX[pt.index()]] += 1;
     }
     pub fn decrement(&mut self, pt: PieceType) {
-        match pt {
-            PieceType::FU | PieceType::TO => self.fu -= 1,
-            PieceType::KY | PieceType::NY => self.ky -= 1,
-            PieceType::KE | PieceType::NK => self.ke -= 1,
-            PieceType::GI | PieceType::NG => self.gi -= 1,
-            PieceType::KI => self.ki -= 1,
-            PieceType::KA | PieceType::UM => self.ka -= 1,
-            PieceType::HI | PieceType::RY => self.hi -= 1,
-            _ => unreachable!(),
-        }
+        self.0[Hand::PIECE_TYPE_INDEX[pt.index()]] -= 1;
     }
 }
