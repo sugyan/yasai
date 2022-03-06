6e652f9f9d1acbccd0c1f2f61944c26ddd1e2c14
sugyan <sugi1982@gmail.com>
2022-03-06 12:36:01 +0900

    Use portable_simd

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,15 +1,14 @@
 use crate::square::{File, Rank};
 use crate::Square;
+use core_simd::u64x2;
 use std::{fmt, ops};
 
 #[derive(Clone, Copy)]
-pub union Bitboard {
-    u: [u64; 2],
-}
+pub struct Bitboard(u64x2);
 
 impl Bitboard {
-    #[rustfmt::skip]    pub const ZERO: Bitboard = Bitboard { u: [                    0,           0] };
-    #[rustfmt::skip]    pub const ONES: Bitboard = Bitboard { u: [0x7fff_ffff_ffff_ffff, 0x0003_ffff] };
+    #[rustfmt::skip]    pub const ZERO: Bitboard = Bitboard(u64x2::from_array([                    0,           0]));
+    #[rustfmt::skip]    pub const ONES: Bitboard = Bitboard(u64x2::from_array([0x7fff_ffff_ffff_ffff, 0x0003_ffff]));
 
     pub fn is_empty(&self) -> bool {
         (self.value(0) | self.value(1)) == 0
@@ -27,7 +26,7 @@ impl Bitboard {
         }
     }
     pub fn value(&self, i: usize) -> u64 {
-        unsafe { self.u[i] }
+        self.0.as_array()[i]
     }
     pub fn from_square(sq: Square) -> Bitboard {
         Bitboard::SQUARE[sq.index()]
@@ -43,126 +42,122 @@ impl Bitboard {
     }
     fn pop0(&mut self) -> Square {
         let sq = Square(self.value(0).trailing_zeros() as i8);
-        unsafe {
-            self.u[0] &= self.u[0] - 1;
-        }
+        self.0.as_mut_array()[0] &= self.value(0) - 1;
         sq
     }
     fn pop1(&mut self) -> Square {
         let sq = Square(self.value(1).trailing_zeros() as i8 + 63);
-        unsafe {
-            self.u[1] &= self.u[1] - 1;
-        }
+        self.0.as_mut_array()[1] &= self.value(1) - 1;
         sq
     }
 
     #[rustfmt::skip]
     pub const FILES: [Bitboard; File::NUM] = [
-        Bitboard { u: [0x01ff      ,            0] },
-        Bitboard { u: [0x01ff <<  9,            0] },
-        Bitboard { u: [0x01ff << 18,            0] },
-        Bitboard { u: [0x01ff << 27,            0] },
-        Bitboard { u: [0x01ff << 36,            0] },
-        Bitboard { u: [0x01ff << 45,            0] },
-        Bitboard { u: [0x01ff << 54,            0] },
-        Bitboard { u: [           0, 0x01ff      ] },
-        Bitboard { u: [           0, 0x01ff <<  9] }
+        Bitboard(u64x2::from_array([0x01ff      ,            0])),
+        Bitboard(u64x2::from_array([0x01ff <<  9,            0])),
+        Bitboard(u64x2::from_array([0x01ff << 18,            0])),
+        Bitboard(u64x2::from_array([0x01ff << 27,            0])),
+        Bitboard(u64x2::from_array([0x01ff << 36,            0])),
+        Bitboard(u64x2::from_array([0x01ff << 45,            0])),
+        Bitboard(u64x2::from_array([0x01ff << 54,            0])),
+        Bitboard(u64x2::from_array([           0, 0x01ff      ])),
+        Bitboard(u64x2::from_array([           0, 0x01ff <<  9])),
     ];
     #[rustfmt::skip]
     pub const RANKS: [Bitboard; Rank::NUM] = [
-        Bitboard { u: [0x0040_2010_0804_0201     , 0x0201     ] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 1, 0x0201 << 1] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 2, 0x0201 << 2] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 3, 0x0201 << 3] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 4, 0x0201 << 4] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 5, 0x0201 << 5] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 6, 0x0201 << 6] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 7, 0x0201 << 7] },
-        Bitboard { u: [0x0040_2010_0804_0201 << 8, 0x0201 << 8] },
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201     , 0x0201     ])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 1, 0x0201 << 1])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 2, 0x0201 << 2])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 3, 0x0201 << 3])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 4, 0x0201 << 4])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 5, 0x0201 << 5])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 6, 0x0201 << 6])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 7, 0x0201 << 7])),
+        Bitboard(u64x2::from_array([0x0040_2010_0804_0201 << 8, 0x0201 << 8])),
     ];
     #[rustfmt::skip]
     const SQUARE: [Bitboard; Square::NUM] = [
-        Bitboard { u: [1 <<  0,       0] },
-        Bitboard { u: [1 <<  1,       0] },
-        Bitboard { u: [1 <<  2,       0] },
-        Bitboard { u: [1 <<  3,       0] },
-        Bitboard { u: [1 <<  4,       0] },
-        Bitboard { u: [1 <<  5,       0] },
-        Bitboard { u: [1 <<  6,       0] },
-        Bitboard { u: [1 <<  7,       0] },
-        Bitboard { u: [1 <<  8,       0] },
-        Bitboard { u: [1 <<  9,       0] },
-        Bitboard { u: [1 << 10,       0] },
-        Bitboard { u: [1 << 11,       0] },
-        Bitboard { u: [1 << 12,       0] },
-        Bitboard { u: [1 << 13,       0] },
-        Bitboard { u: [1 << 14,       0] },
-        Bitboard { u: [1 << 15,       0] },
-        Bitboard { u: [1 << 16,       0] },
-        Bitboard { u: [1 << 17,       0] },
-        Bitboard { u: [1 << 18,       0] },
-        Bitboard { u: [1 << 19,       0] },
-        Bitboard { u: [1 << 20,       0] },
-        Bitboard { u: [1 << 21,       0] },
-        Bitboard { u: [1 << 22,       0] },
-        Bitboard { u: [1 << 23,       0] },
-        Bitboard { u: [1 << 24,       0] },
-        Bitboard { u: [1 << 25,       0] },
-        Bitboard { u: [1 << 26,       0] },
-        Bitboard { u: [1 << 27,       0] },
-        Bitboard { u: [1 << 28,       0] },
-        Bitboard { u: [1 << 29,       0] },
-        Bitboard { u: [1 << 30,       0] },
-        Bitboard { u: [1 << 31,       0] },
-        Bitboard { u: [1 << 32,       0] },
-        Bitboard { u: [1 << 33,       0] },
-        Bitboard { u: [1 << 34,       0] },
-        Bitboard { u: [1 << 35,       0] },
-        Bitboard { u: [1 << 36,       0] },
-        Bitboard { u: [1 << 37,       0] },
-        Bitboard { u: [1 << 38,       0] },
-        Bitboard { u: [1 << 39,       0] },
-        Bitboard { u: [1 << 40,       0] },
-        Bitboard { u: [1 << 41,       0] },
-        Bitboard { u: [1 << 42,       0] },
-        Bitboard { u: [1 << 43,       0] },
-        Bitboard { u: [1 << 44,       0] },
-        Bitboard { u: [1 << 45,       0] },
-        Bitboard { u: [1 << 46,       0] },
-        Bitboard { u: [1 << 47,       0] },
-        Bitboard { u: [1 << 48,       0] },
-        Bitboard { u: [1 << 49,       0] },
-        Bitboard { u: [1 << 50,       0] },
-        Bitboard { u: [1 << 51,       0] },
-        Bitboard { u: [1 << 52,       0] },
-        Bitboard { u: [1 << 53,       0] },
-        Bitboard { u: [1 << 54,       0] },
-        Bitboard { u: [1 << 55,       0] },
-        Bitboard { u: [1 << 56,       0] },
-        Bitboard { u: [1 << 57,       0] },
-        Bitboard { u: [1 << 58,       0] },
-        Bitboard { u: [1 << 59,       0] },
-        Bitboard { u: [1 << 60,       0] },
-        Bitboard { u: [1 << 61,       0] },
-        Bitboard { u: [1 << 62,       0] },
-        Bitboard { u: [      0, 1 <<  0] },
-        Bitboard { u: [      0, 1 <<  1] },
-        Bitboard { u: [      0, 1 <<  2] },
-        Bitboard { u: [      0, 1 <<  3] },
-        Bitboard { u: [      0, 1 <<  4] },
-        Bitboard { u: [      0, 1 <<  5] },
-        Bitboard { u: [      0, 1 <<  6] },
-        Bitboard { u: [      0, 1 <<  7] },
-        Bitboard { u: [      0, 1 <<  8] },
-        Bitboard { u: [      0, 1 <<  9] },
-        Bitboard { u: [      0, 1 << 10] },
-        Bitboard { u: [      0, 1 << 11] },
-        Bitboard { u: [      0, 1 << 12] },
-        Bitboard { u: [      0, 1 << 13] },
-        Bitboard { u: [      0, 1 << 14] },
-        Bitboard { u: [      0, 1 << 15] },
-        Bitboard { u: [      0, 1 << 16] },
-        Bitboard { u: [      0, 1 << 17] },
+        Bitboard(u64x2::from_array([1 <<  0,       0])),
+        Bitboard(u64x2::from_array([1 <<  1,       0])),
+        Bitboard(u64x2::from_array([1 <<  2,       0])),
+        Bitboard(u64x2::from_array([1 <<  3,       0])),
+        Bitboard(u64x2::from_array([1 <<  4,       0])),
+        Bitboard(u64x2::from_array([1 <<  5,       0])),
+        Bitboard(u64x2::from_array([1 <<  6,       0])),
+        Bitboard(u64x2::from_array([1 <<  7,       0])),
+        Bitboard(u64x2::from_array([1 <<  8,       0])),
+        Bitboard(u64x2::from_array([1 <<  9,       0])),
+        Bitboard(u64x2::from_array([1 << 10,       0])),
+        Bitboard(u64x2::from_array([1 << 11,       0])),
+        Bitboard(u64x2::from_array([1 << 12,       0])),
+        Bitboard(u64x2::from_array([1 << 13,       0])),
+        Bitboard(u64x2::from_array([1 << 14,       0])),
+        Bitboard(u64x2::from_array([1 << 15,       0])),
+        Bitboard(u64x2::from_array([1 << 16,       0])),
+        Bitboard(u64x2::from_array([1 << 17,       0])),
+        Bitboard(u64x2::from_array([1 << 18,       0])),
+        Bitboard(u64x2::from_array([1 << 19,       0])),
+        Bitboard(u64x2::from_array([1 << 20,       0])),
+        Bitboard(u64x2::from_array([1 << 21,       0])),
+        Bitboard(u64x2::from_array([1 << 22,       0])),
+        Bitboard(u64x2::from_array([1 << 23,       0])),
+        Bitboard(u64x2::from_array([1 << 24,       0])),
+        Bitboard(u64x2::from_array([1 << 25,       0])),
+        Bitboard(u64x2::from_array([1 << 26,       0])),
+        Bitboard(u64x2::from_array([1 << 27,       0])),
+        Bitboard(u64x2::from_array([1 << 28,       0])),
+        Bitboard(u64x2::from_array([1 << 29,       0])),
+        Bitboard(u64x2::from_array([1 << 30,       0])),
+        Bitboard(u64x2::from_array([1 << 31,       0])),
+        Bitboard(u64x2::from_array([1 << 32,       0])),
+        Bitboard(u64x2::from_array([1 << 33,       0])),
+        Bitboard(u64x2::from_array([1 << 34,       0])),
+        Bitboard(u64x2::from_array([1 << 35,       0])),
+        Bitboard(u64x2::from_array([1 << 36,       0])),
+        Bitboard(u64x2::from_array([1 << 37,       0])),
+        Bitboard(u64x2::from_array([1 << 38,       0])),
+        Bitboard(u64x2::from_array([1 << 39,       0])),
+        Bitboard(u64x2::from_array([1 << 40,       0])),
+        Bitboard(u64x2::from_array([1 << 41,       0])),
+        Bitboard(u64x2::from_array([1 << 42,       0])),
+        Bitboard(u64x2::from_array([1 << 43,       0])),
+        Bitboard(u64x2::from_array([1 << 44,       0])),
+        Bitboard(u64x2::from_array([1 << 45,       0])),
+        Bitboard(u64x2::from_array([1 << 46,       0])),
+        Bitboard(u64x2::from_array([1 << 47,       0])),
+        Bitboard(u64x2::from_array([1 << 48,       0])),
+        Bitboard(u64x2::from_array([1 << 49,       0])),
+        Bitboard(u64x2::from_array([1 << 50,       0])),
+        Bitboard(u64x2::from_array([1 << 51,       0])),
+        Bitboard(u64x2::from_array([1 << 52,       0])),
+        Bitboard(u64x2::from_array([1 << 53,       0])),
+        Bitboard(u64x2::from_array([1 << 54,       0])),
+        Bitboard(u64x2::from_array([1 << 55,       0])),
+        Bitboard(u64x2::from_array([1 << 56,       0])),
+        Bitboard(u64x2::from_array([1 << 57,       0])),
+        Bitboard(u64x2::from_array([1 << 58,       0])),
+        Bitboard(u64x2::from_array([1 << 59,       0])),
+        Bitboard(u64x2::from_array([1 << 60,       0])),
+        Bitboard(u64x2::from_array([1 << 61,       0])),
+        Bitboard(u64x2::from_array([1 << 62,       0])),
+        Bitboard(u64x2::from_array([      0, 1 <<  0])),
+        Bitboard(u64x2::from_array([      0, 1 <<  1])),
+        Bitboard(u64x2::from_array([      0, 1 <<  2])),
+        Bitboard(u64x2::from_array([      0, 1 <<  3])),
+        Bitboard(u64x2::from_array([      0, 1 <<  4])),
+        Bitboard(u64x2::from_array([      0, 1 <<  5])),
+        Bitboard(u64x2::from_array([      0, 1 <<  6])),
+        Bitboard(u64x2::from_array([      0, 1 <<  7])),
+        Bitboard(u64x2::from_array([      0, 1 <<  8])),
+        Bitboard(u64x2::from_array([      0, 1 <<  9])),
+        Bitboard(u64x2::from_array([      0, 1 << 10])),
+        Bitboard(u64x2::from_array([      0, 1 << 11])),
+        Bitboard(u64x2::from_array([      0, 1 << 12])),
+        Bitboard(u64x2::from_array([      0, 1 << 13])),
+        Bitboard(u64x2::from_array([      0, 1 << 14])),
+        Bitboard(u64x2::from_array([      0, 1 << 15])),
+        Bitboard(u64x2::from_array([      0, 1 << 16])),
+        Bitboard(u64x2::from_array([      0, 1 << 17])),
     ];
 }
 
@@ -170,9 +165,7 @@ impl ops::Not for Bitboard {
     type Output = Bitboard;
 
     fn not(self) -> Self::Output {
-        Bitboard {
-            u: [!self.value(0), !self.value(1)],
-        }
+        Bitboard(!self.0)
     }
 }
 
@@ -180,9 +173,7 @@ impl ops::BitAnd<Bitboard> for Bitboard {
     type Output = Bitboard;
 
     fn bitand(self, rhs: Bitboard) -> Self::Output {
-        Bitboard {
-            u: [self.value(0) & rhs.value(0), self.value(1) & rhs.value(1)],
-        }
+        Bitboard(self.0 & rhs.0)
     }
 }
 
@@ -198,9 +189,7 @@ impl ops::BitOr<Bitboard> for Bitboard {
     type Output = Bitboard;
 
     fn bitor(self, rhs: Bitboard) -> Self::Output {
-        Bitboard {
-            u: [self.value(0) | rhs.value(0), self.value(1) | rhs.value(1)],
-        }
+        Bitboard(self.0 | rhs.0)
     }
 }
 
@@ -214,10 +203,7 @@ impl ops::BitOr<Square> for Bitboard {
 
 impl ops::BitAndAssign<Bitboard> for Bitboard {
     fn bitand_assign(&mut self, rhs: Bitboard) {
-        unsafe {
-            self.u[0] &= rhs.u[0];
-            self.u[1] &= rhs.u[1];
-        }
+        self.0 &= rhs.0;
     }
 }
 
@@ -229,10 +215,7 @@ impl ops::BitAndAssign<Square> for Bitboard {
 
 impl ops::BitOrAssign<Bitboard> for Bitboard {
     fn bitor_assign(&mut self, rhs: Bitboard) {
-        unsafe {
-            self.u[0] |= rhs.u[0];
-            self.u[1] |= rhs.u[1];
-        }
+        self.0 |= rhs.0;
     }
 }
 
@@ -244,10 +227,7 @@ impl ops::BitOrAssign<Square> for Bitboard {
 
 impl ops::BitXorAssign<Bitboard> for Bitboard {
     fn bitxor_assign(&mut self, rhs: Bitboard) {
-        unsafe {
-            self.u[0] ^= rhs.u[0];
-            self.u[1] ^= rhs.u[1];
-        }
+        self.0 ^= rhs.0;
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(portable_simd)]
+
 mod bitboard;
 mod color;
 mod hand;
