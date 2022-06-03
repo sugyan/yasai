0eda85d884ec64336c937a053f0d51f8c5b7cde1
github-action-benchmark <github@users.noreply.github.com>
2022-06-03 15:37:05 +0000

    add Benchmark (cargo) benchmark result for 28614edb9575df414150aeac317f560279c91e97

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1654270625084,
+  "repoUrl": "https://github.com/sugyan/yasai",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "sugyan",
+            "username": "sugyan"
+          },
+          "committer": {
+            "name": "sugyan",
+            "username": "sugyan"
+          },
+          "id": "28614edb9575df414150aeac317f560279c91e97",
+          "message": "Use shogi_core",
+          "timestamp": "2022-06-02T11:42:34Z",
+          "url": "https://github.com/sugyan/yasai/pull/7/commits/28614edb9575df414150aeac317f560279c91e97"
+        },
+        "date": 1654270624677,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "perft::bench_perft_5_from_default",
+            "value": 796994781,
+            "range": "± 85166811",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
