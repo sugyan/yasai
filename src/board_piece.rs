6fa95edaec6ade349c53b714beeeeeec893c696f
sugyan <sugi1982@gmail.com>
2022-06-06 00:11:00 +0900

    Remove unused types

--- src/board_piece.rs
+++ src/board_piece.rs
@@ -1,60 +1,60 @@
 use shogi_core::Piece;
 
 #[allow(dead_code)]
-pub(crate) static EMP: Option<Piece> = None;
+pub(crate) const EMP: Option<Piece> = None;
 #[allow(dead_code)]
-pub(crate) static BFU: Option<Piece> = Some(Piece::B_P);
+pub(crate) const BFU: Option<Piece> = Some(Piece::B_P);
 #[allow(dead_code)]
-pub(crate) static BKY: Option<Piece> = Some(Piece::B_L);
+pub(crate) const BKY: Option<Piece> = Some(Piece::B_L);
 #[allow(dead_code)]
-pub(crate) static BKE: Option<Piece> = Some(Piece::B_N);
+pub(crate) const BKE: Option<Piece> = Some(Piece::B_N);
 #[allow(dead_code)]
-pub(crate) static BGI: Option<Piece> = Some(Piece::B_S);
+pub(crate) const BGI: Option<Piece> = Some(Piece::B_S);
 #[allow(dead_code)]
-pub(crate) static BKA: Option<Piece> = Some(Piece::B_B);
+pub(crate) const BKA: Option<Piece> = Some(Piece::B_B);
 #[allow(dead_code)]
-pub(crate) static BHI: Option<Piece> = Some(Piece::B_R);
+pub(crate) const BHI: Option<Piece> = Some(Piece::B_R);
 #[allow(dead_code)]
-pub(crate) static BKI: Option<Piece> = Some(Piece::B_G);
+pub(crate) const BKI: Option<Piece> = Some(Piece::B_G);
 #[allow(dead_code)]
-pub(crate) static BOU: Option<Piece> = Some(Piece::B_K);
+pub(crate) const BOU: Option<Piece> = Some(Piece::B_K);
 #[allow(dead_code)]
-pub(crate) static BTO: Option<Piece> = Some(Piece::B_PP);
+pub(crate) const BTO: Option<Piece> = Some(Piece::B_PP);
 #[allow(dead_code)]
-pub(crate) static BNY: Option<Piece> = Some(Piece::B_PL);
+pub(crate) const BNY: Option<Piece> = Some(Piece::B_PL);
 #[allow(dead_code)]
-pub(crate) static BNK: Option<Piece> = Some(Piece::B_PN);
+pub(crate) const BNK: Option<Piece> = Some(Piece::B_PN);
 #[allow(dead_code)]
-pub(crate) static BNG: Option<Piece> = Some(Piece::B_PS);
+pub(crate) const BNG: Option<Piece> = Some(Piece::B_PS);
 #[allow(dead_code)]
-pub(crate) static BUM: Option<Piece> = Some(Piece::B_PB);
+pub(crate) const BUM: Option<Piece> = Some(Piece::B_PB);
 #[allow(dead_code)]
-pub(crate) static BRY: Option<Piece> = Some(Piece::B_PR);
+pub(crate) const BRY: Option<Piece> = Some(Piece::B_PR);
 #[allow(dead_code)]
-pub(crate) static WFU: Option<Piece> = Some(Piece::W_P);
+pub(crate) const WFU: Option<Piece> = Some(Piece::W_P);
 #[allow(dead_code)]
-pub(crate) static WKY: Option<Piece> = Some(Piece::W_L);
+pub(crate) const WKY: Option<Piece> = Some(Piece::W_L);
 #[allow(dead_code)]
-pub(crate) static WKE: Option<Piece> = Some(Piece::W_N);
+pub(crate) const WKE: Option<Piece> = Some(Piece::W_N);
 #[allow(dead_code)]
-pub(crate) static WGI: Option<Piece> = Some(Piece::W_S);
+pub(crate) const WGI: Option<Piece> = Some(Piece::W_S);
 #[allow(dead_code)]
-pub(crate) static WKA: Option<Piece> = Some(Piece::W_B);
+pub(crate) const WKA: Option<Piece> = Some(Piece::W_B);
 #[allow(dead_code)]
-pub(crate) static WHI: Option<Piece> = Some(Piece::W_R);
+pub(crate) const WHI: Option<Piece> = Some(Piece::W_R);
 #[allow(dead_code)]
-pub(crate) static WKI: Option<Piece> = Some(Piece::W_G);
+pub(crate) const WKI: Option<Piece> = Some(Piece::W_G);
 #[allow(dead_code)]
-pub(crate) static WOU: Option<Piece> = Some(Piece::W_K);
+pub(crate) const WOU: Option<Piece> = Some(Piece::W_K);
 #[allow(dead_code)]
-pub(crate) static WTO: Option<Piece> = Some(Piece::W_PP);
+pub(crate) const WTO: Option<Piece> = Some(Piece::W_PP);
 #[allow(dead_code)]
-pub(crate) static WNY: Option<Piece> = Some(Piece::W_PL);
+pub(crate) const WNY: Option<Piece> = Some(Piece::W_PL);
 #[allow(dead_code)]
-pub(crate) static WNK: Option<Piece> = Some(Piece::W_PN);
+pub(crate) const WNK: Option<Piece> = Some(Piece::W_PN);
 #[allow(dead_code)]
-pub(crate) static WNG: Option<Piece> = Some(Piece::W_PS);
+pub(crate) const WNG: Option<Piece> = Some(Piece::W_PS);
 #[allow(dead_code)]
-pub(crate) static WUM: Option<Piece> = Some(Piece::W_PB);
+pub(crate) const WUM: Option<Piece> = Some(Piece::W_PB);
 #[allow(dead_code)]
-pub(crate) static WRY: Option<Piece> = Some(Piece::W_PR);
+pub(crate) const WRY: Option<Piece> = Some(Piece::W_PR);
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@ mod board_piece;
 mod movegen;
 mod position;
 mod shogi_move;
-mod square;
 mod tables;
 pub mod utils;
 mod zobrist;
 
 pub use position::Position;
 pub use shogi_move::{Move, MoveType};
-pub use square::{File, Rank, Square};
