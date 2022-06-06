9e650e2e3537b71b888eb6fc5d666b9d1ed22c29
github-action-benchmark <github@users.noreply.github.com>
2022-06-06 14:07:33 +0000

    add Benchmark (cargo) benchmark result for 60f2bd5be8a33b97187634507e0de0c27ff423b2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654508446807,
+  "lastUpdate": 1654524453337,
   "repoUrl": "https://github.com/sugyan/yasai",
   "entries": {
     "Benchmark": [
@@ -103,6 +103,36 @@ window.BENCHMARK_DATA = {
             "name": "perft::bench_perft_5_from_default",
             "value": 443838101,
             "range": "± 1451469",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sugi1982@gmail.com",
+            "name": "Yoshihiro Sugi",
+            "username": "sugyan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "60f2bd5be8a33b97187634507e0de0c27ff423b2",
+          "message": "Use shogi_core (#7)\n\n* Use shogi_core, replace Color\r\n\r\n* Replace Piece, Hand to shogi_core\r\n\r\n* Update zobrist keys for hands\r\n\r\n* Updaate shogi_core to `0.1.2`\r\n\r\n* Use Piece constants\r\n\r\n* Replace Bitboard and Square to shogi_core\r\n\r\n* Fix benches\r\n\r\n* Use Bitboard::contains\r\n\r\n* Update workflow\r\n\r\n* Update rust.yml\r\n\r\n* Update shogi_core to `0.1.3`\r\n\r\n* Update: micro-optimizations\r\n\r\n* Remove unused types\r\n\r\n* Replace Move to shogi_core\r\n\r\n* Remove shogi_move.rs\r\n\r\n* Fix workflow",
+          "timestamp": "2022-06-06T23:03:31+09:00",
+          "tree_id": "56618f7af17b7374c92b98b85445ed3c399381d2",
+          "url": "https://github.com/sugyan/yasai/commit/60f2bd5be8a33b97187634507e0de0c27ff423b2"
+        },
+        "date": 1654524452926,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "perft::bench_perft_5_from_default",
+            "value": 404256297,
+            "range": "± 44090803",
             "unit": "ns/iter"
           }
         ]
